0412cd4a207f9a61be5a678943eddcf20bc47f48
Romit Choudhary <choudharyromit@gmail.com>
2020-10-09 15:17:08 +0530

    Updates

---
+++ precache-manifest.775c0be627ce3c570b8afe92592345f3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab25fb616f82ba8325818757191bbcc",
+    "url": "/klaus-app/index.html"
+  },
+  {
+    "revision": "6b3959e7cac2ac7f1157",
+    "url": "/klaus-app/static/css/main.b85d1ead.chunk.css"
+  },
+  {
+    "revision": "70359be633e531edb81f",
+    "url": "/klaus-app/static/js/2.afd25754.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/klaus-app/static/js/2.afd25754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b3959e7cac2ac7f1157",
+    "url": "/klaus-app/static/js/main.994d091e.chunk.js"
+  },
+  {
+    "revision": "bed71e02db5f32485ec2",
+    "url": "/klaus-app/static/js/runtime-main.6e866538.js"
+  },
+  {
+    "revision": "d6079ef01292c4bc84dce33988641530",
+    "url": "/klaus-app/static/media/SFProText-Bold.d6079ef0.ttf"
+  },
+  {
+    "revision": "70830fa4b0f259734f7e52903f3efd59",
+    "url": "/klaus-app/static/media/SFProText-Medium.70830fa4.ttf"
+  },
+  {
+    "revision": "85bd46c1cff02c1d8360cc714b8298fa",
+    "url": "/klaus-app/static/media/SFProText-Regular.85bd46c1.ttf"
+  }
+]);
